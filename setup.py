1c58c45ab6d618b68c0fa6421c85645b0d61207c
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-03-24 18:31:01 +0100

    Bump version, update setuptools info and add `vspreview` console script

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ with open("requirements.txt", "r", encoding="UTF-8") as rq:
 
 setup(
     name="vspreview",
-    version="0.1b",
+    version="0.2b",
     author="Endilll",
+    maintainer='Setsugennoao',
+    maintainer_email='setsugen@setsugen.dev',
     description="Preview for VapourSynth scripts",
     long_description=long_desc,
     long_description_content_type="text/markdown",
     url="https://github.com/Setsugennoao/vapoursynth-preview",
-    packages=find_packages(exclude=("docs", "stubs")),
+    packages=find_packages('.', ("docs", "stubs")),
     install_requires=req,
     python_requires=">=3.9",
     zip_safe=False,
@@ -34,5 +36,10 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    entry_points={
+        'console_scripts': [
+            'vspreview = vspreview.init:main'
+        ]
+    }
 )

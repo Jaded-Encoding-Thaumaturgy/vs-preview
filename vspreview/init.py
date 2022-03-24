79adf8cb305744404c2719f88ff4d04c4382f3a0
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-03-24 22:57:17 +0100

    Prettier logs

--- vspreview/init.py
+++ vspreview/init.py
@@ -38,6 +38,19 @@ class Application(QApplication):
 def main() -> None:
     logging.basicConfig(format='{asctime}: {levelname}: {message}', style='{', level=MainWindow.LOG_LEVEL)
     logging.Formatter.default_msec_format = '%s.%03d'
+    if sys.stdout.isatty():
+        logging.addLevelName(
+            logging.DEBUG, "\033[0;32m%s\033[0m" % logging.getLevelName(logging.DEBUG)
+        )
+        logging.addLevelName(
+            logging.INFO, "\033[1;33m%s\033[0m" % logging.getLevelName(logging.INFO)
+        )
+        logging.addLevelName(
+            logging.WARNING, "\033[1;35m%s\033[1;0m" % logging.getLevelName(logging.WARNING)
+        )
+        logging.addLevelName(
+            logging.ERROR, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.ERROR)
+        )
 
     check_versions()
 

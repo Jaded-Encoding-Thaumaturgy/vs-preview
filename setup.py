2df6585fd617e96e03fd2225c8f26c026accec29
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-05-29 21:21:29 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as fh:
 
 setup(
     name="vspreview",
-    version="0.2.3a0",
+    version="0.2.4",
     author="Endilll",
     maintainer='Setsugennoao',
     maintainer_email='setsugen@setsugen.dev',

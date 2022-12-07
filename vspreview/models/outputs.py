e1c928afd571bc9a2e3176c7ab556b2c843599d5
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-07 14:05:39 +0100

    Pass timecodesb when instantiating the VideoOutput

--- vspreview/models/outputs.py
+++ vspreview/models/outputs.py
@@ -22,6 +22,7 @@ class Outputs(Generic[T], QAbstractListModel, QYAMLObject):
     def setValue(self, main: AbstractMainWindow, local_storage: Mapping[str, T] | None = None) -> None:
         super().__init__()
         self.items = list[T]()
+        self.main = main
 
         local_storage, newstorage = (local_storage, False) if local_storage is not None else ({}, True)
 
@@ -39,7 +40,10 @@ class Outputs(Generic[T], QAbstractListModel, QYAMLObject):
                 output = local_storage[str(i)]
                 output.setValue(vs_output, i, newstorage)
             except KeyError:
-                output = self.out_type(vs_output, i, newstorage)
+                if self.out_type is VideoOutput and i in self.main.timecodes:
+                    output = self.out_type(vs_output, i, newstorage, self.main.timecodes[i])
+                else:
+                    output = self.out_type(vs_output, i, newstorage)
 
             self.items.append(output)
 

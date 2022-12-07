8f509f15019bd665a6e9e08a4cfacaadad1c6fda
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-07 14:04:23 +0100

    Update `set_timecodes`

--- vspreview/__init__.py
+++ vspreview/__init__.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
 from fractions import Fraction
-from typing import Dict, List, Tuple
 
 from .init import main  # noqa: F401
 
 
-def set_timecodes(index: int, timecodes: str | Dict[Tuple[int | None, int | None], Fraction] | List[int]) -> None:
+def set_timecodes(index: int, timecodes: str | dict[tuple[int | None, int | None], Fraction] | list[int]) -> None:
     from .core import main_window
 
-    main_window()
+    main_window().update_timecodes_info(index, timecodes)

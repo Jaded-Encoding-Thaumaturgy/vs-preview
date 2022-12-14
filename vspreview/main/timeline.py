8289c78c4f4e0879dda32131d12c02a6e3950840
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-14 17:32:01 +0100

    Update `Timeline.set_end_frame`

--- vspreview/main/timeline.py
+++ vspreview/main/timeline.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import QEvent, QLineF, QPoint, QRectF, Qt, pyqtSignal
 from PyQt5.QtGui import QColor, QMouseEvent, QMoveEvent, QPainter, QPaintEvent, QPalette, QPen, QResizeEvent
 from PyQt5.QtWidgets import QApplication, QToolTip, QWidget
 
-from ..core import AbstractToolbar, AbstractYAMLObject, Frame, Scene, Time, main_window
+from ..core import AbstractToolbar, AbstractYAMLObject, Frame, Scene, Time, main_window, VideoOutput
 from ..utils import strfdelta
 
 
@@ -347,9 +347,9 @@ class Timeline(QWidget):
         else:
             return '%m:%S'
 
-    def set_end_frame(self, end_f: Frame) -> None:
-        self.end_f = end_f
-        self.end_t = Time(end_f)
+    def set_end_frame(self, node: VideoOutput) -> None:
+        self.end_f = node.total_frames
+        self.end_t = node.total_time
         self.full_repaint()
 
     def set_position(self, pos: Frame | Time | int) -> None:

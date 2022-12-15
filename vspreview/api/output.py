08178795d232190ebc3a09329e55702342ff3122
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-15 14:42:29 +0100

    Update set_output

--- vspreview/api/output.py
+++ vspreview/api/output.py
@@ -16,7 +16,7 @@ __all__ = [
 
 
 def set_output(
-    node: vs.RawNode, name: str | None = None,
+    node: vs.RawNode, name: str | bool | None = None,
     *, timecodes: str | Path | dict[
         tuple[int | None, int | None], float | tuple[int, int] | Fraction
     ] | list[Fraction] | None = None, denominator: int = 1001
@@ -32,7 +32,7 @@ def set_output(
     else:
         title, node_type = 'Node', vs.RawNode
 
-    if not name:
+    if not name or name is True:
         name = f"{title} {index}"
 
         current_frame = inspect.currentframe()
@@ -50,7 +50,8 @@ def set_output(
     if is_preview():
         from ..core import main_window
 
-        main_window().set_node_name(node_type, index, name.title())  # type: ignore
+        if isinstance(name, str):
+            main_window().set_node_name(node_type, index, name.title())  # type: ignore
 
         if timecodes:
             set_timecodes(index, timecodes, (

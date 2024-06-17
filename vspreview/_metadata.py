8210f0b0e2de6147467d4450de68b1d2fb9996dc
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2024-06-17 15:46:35 +0200

    Bump requirements and major version

--- vspreview/_metadata.py
+++ vspreview/_metadata.py
@@ -1,6 +1,6 @@
 """Previewer for VapourSynth scripts"""
 
-__version__ = '0.8.1'
+__version__ = '0.9.0'
 
 __author_name__, __author_email__ = 'Endilll', ''
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'

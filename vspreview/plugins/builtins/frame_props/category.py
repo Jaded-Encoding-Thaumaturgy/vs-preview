71a8e057f37c4e39c4aab6a379f86dfd7f0ed431
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-09-30 12:20:37 -0700

    Overhaul frame props plugin (#182)
    * Add categorisation, cosmetic updates, etc.
    
    * Add `idx_filepath` to Video category
    
    * Add `dgi_` to Metrics
    
    * Add lut typing
    
    * Rename Fields category to Field
    
    * Double-click to copy row value
    
    * Strip properties in prettify window
    
    * Add `pkt_` frameprops to Metrics
    
    * Minor tooltip update
    
    * Split off LUTs and the like, add more checks, fixes
    
    * Minor changes
    
    * Update props
    
    - https://github.com/Jaded-Encoding-Thaumaturgy/vs-tools/commit/bd489def354855bef7da3b2907e52c7976a21b50
    - https://github.com/Jaded-Encoding-Thaumaturgy/vs-source/commit/f1ed7d047bad978407556b25d784ab491459a1e8

---
+++ vspreview/plugins/builtins/frame_props/category.py
@@ -0,0 +1,84 @@
+__all__ = [
+    'frame_props_categories',
+    'frame_props_category_prefix_lut',
+    'frame_props_category_suffix_lut',
+]
+
+video_props: list[str] = [
+    # Colorimetry
+    '_Matrix', '_Transfer', '_Primaries',
+    '_ChromaLocation', '_ColorRange',
+    # Frame Properties
+    '_DurationNum', '_DurationDen', '_AbsoluteTime',
+    '_SARNum', '_SARDen',
+    '_FieldBased',
+    # Other
+    '_FrameNumber', '_Alpha', 'Name', 'IdxFilePath',
+]
+"""Properties related to video colorimetry, frame characteristics, and other video-specific information."""
+
+metrics_props: list[str] = [
+    # Scene detection
+    '_SceneChangeNext', '_SceneChangePrev', 'SceneChange',
+    # DMetrics
+    'MMetrics', 'VMetrics',
+]
+"""Properties related to video analysis metrics and statistics."""
+
+field_props: list[str] = [
+    '_Combed', '_Field', '_FieldBased',
+]
+"""Properties specific to interlaced video and field-based processing."""
+
+
+other_props: list[str] = []
+"""Props that don't fit into other categories or haven't been sorted"""
+
+frame_props_categories: dict[str, list[str]] = {
+    'Video': video_props,
+    'Metrics': metrics_props,
+    'Field': field_props,
+    'Other': other_props
+}
+
+frame_props_category_prefix_lut: dict[str, str] = {
+    str(prefix): str(category) for category, prefixes in {
+        'Metrics': (
+            # PlaneStats
+            'PlaneStats', 'psm', 'VFMPlaneStats',
+            # DGIndex (via vssource)
+            'Dgi',
+            # Auto-balancing (via stgfunc)
+            'AutoBalance',
+            # Scene-based graining (via lvsfunc)
+            'SceneGrain',
+            # Packet sizes (via lvsfunc)
+            'Pkt',
+            # VMAF
+            'ciede2000', 'psnr_',
+            # SSIMULACRA2
+            '_SSIMULACRA2',
+        ),
+        'Field': (
+            # VIVTC
+            'VFM', 'VDecimate',
+            # TIVTC
+            'TFM', 'TDecimate',
+            # Wobbly (via lvsfunc)
+            'wobbly_'
+        )
+    }.items()
+    for prefix in prefixes
+}
+"""LUT for categorizing frame properties based on prefixes."""
+
+frame_props_category_suffix_lut: dict[str, str] = {
+    str(suffix): str(category) for category, suffixes in {
+        'Metrics': (
+            # VMAF
+            '_ssim',
+        )
+    }.items()
+    for suffix in suffixes
+}
+"""LUT for categorizing frame properties based on suffixes."""
---
+++ vspreview/plugins/builtins/frame_props/lut.py
@@ -0,0 +1,259 @@
+from __future__ import annotations
+
+import math
+from typing import Any, Callable
+from vstools import ChromaLocation, ColorRange, FieldBased, Matrix, Primaries, PropEnum, SPath, Transfer
+
+__all__ = [
+    'frame_props_lut'
+]
+
+
+def _create_enum_props_lut(enum: type[PropEnum], pretty_name: str) -> tuple[str, dict[str, dict[int, str]]]:
+    return enum.prop_key, {
+        pretty_name: {
+            idx: enum.from_param(idx).pretty_string if enum.is_valid(idx) else 'Invalid'
+            for idx in range(min(enum.__members__.values()) - 1, max(enum.__members__.values()) + 1)
+        }
+    }
+
+
+# Utils
+def _handle_na(value: int) -> str:
+    return "N/A" if value < 0 else str(value)
+
+
+def _handle_nan(value: Any) -> str:
+    if isinstance(value, float) and math.isnan(value):
+        return 'NaN'
+    return str(value)
+
+
+# Basic frame properties
+basic_frame_props_lut: dict[str, dict[str, list[str] | Callable[[Any], str]]] = {
+    '_Combed': {
+        'Is Combed': [
+            'No',
+            'Yes'
+        ]
+    },
+    '_Field': {
+        'Frame Field Type': [
+            'Bottom Field',
+            'Top Field'
+        ]
+    },
+    '_SceneChangeNext': {
+        'Scene Cut': [
+            'Current Scene',
+            'End of Scene'
+        ]
+    },
+    '_SceneChangePrev': {
+        'Scene Change': [
+            'Current Scene',
+            'Start of Scene'
+        ]
+    },
+}
+
+# VIVTC-related properties
+vivtc_props_lut: dict[str, dict[str, list[str] | Callable[[Any], str]]] = {
+    'VFMMics': {
+        'VFM Mics': lambda mics: ' | '.join(
+            f'{key}: {_handle_na(value)}' for key, value in zip('pcnbu', mics)
+        )
+    },
+    'VFMMatch': {
+        'VFM Match': [
+            'p',
+            'c',
+            'n',
+            'b',
+            'u'
+        ]
+    },
+    'VFMSceneChange': {
+        'VFM Scene Start/End': [
+            'No',
+            'Yes'
+        ]
+    },
+    'VDecimateDrop': {
+        'VDecimate Drop Frame': [
+            'No',
+            'Yes'
+        ]
+    },
+    'VDecimateMaxBlockDiff': {
+        'VDecimate Max Block Diff': lambda max_block_diff: str(max_block_diff)
+    },
+    'VDecimateTotalDiff': {
+        'VDecimate Absolute Total Diff': lambda total_diff: str(total_diff)
+    },
+}
+
+# TIVTC-related properties
+tivtc_props_lut: dict[str, dict[str, list[str] | Callable[[Any], str]]] = {
+    'TFMMics': {
+        'TFM Mics': lambda mics: ' | '.join(
+            f'{key}: {_handle_na(value)}' for key, value in zip('pcnbu', mics)
+        )
+    },
+    'TFMMatch': {
+        'TFM Match': [
+            'p',
+            'c',
+            'n',
+            'b',
+            'u'
+        ]
+    },
+}
+
+# DMetrics-related properties
+dmetrics_props_lut: dict[str, dict[str, Callable[[Any], str]]] = {
+    'MMetrics': {
+        'DMetrics MMetrics': lambda metrics: str(metrics)
+    },
+    'VMetrics': {
+        'DMetrics VMetrics': lambda metrics: str(metrics)
+    },
+}
+
+# Packet size properties
+packet_size_props_lut: dict[str, dict[str, Callable[[Any], str]]] = {
+    'PktSize': {
+        'Packet Size': lambda size: _handle_na(size)
+    },
+    'PktSceneAvgSize': {
+        'Packet Scene Average Size': lambda size: _handle_na(size)
+    },
+    'PktSceneMinSize': {
+        'Packet Scene Minimum Size': lambda size: _handle_na(size)
+    },
+    'PktSceneMaxSize': {
+        'Packet Scene Maximum Size': lambda size: _handle_na(size)
+    },
+}
+
+# vssource properties
+vssource_props_lut: dict[str, dict[str, Callable[[Any], str]]] = {
+    'IdxFilePath': {
+        'Path to File': lambda filepath: SPath(filepath).as_posix()
+    },
+    # DGIndex(NV)
+    'DgiFieldOp': {
+        'DGIndex Field Operation': lambda dgi: str(dgi)
+    },
+    'DgiOrder': {
+        'DGIndex Field Order': lambda dgi: str(dgi)
+    },
+    'DgiFilm': {
+        'DGIndex FILM': lambda dgi: str(dgi) + '%'
+    },
+}
+
+# Wobbly Parser properties
+wobbly_props_lut: dict[str, dict[str, list[str] | Callable[[Any], str]]] = {
+    'wobbly_match': {
+        'Wobbly Match': [
+            'p',
+            'c',
+            'n',
+            'b',
+            'u'
+        ]
+    },
+    'wobbly_combed': {
+        'Wobbly Combed': [
+            'Yes',
+            'No'
+        ]
+    },
+    'wobbly_cycle_fps': {
+        'Wobbly Cycle Framerate': lambda fps: str(fps)
+    },
+    'wobbly_freeze_start': {
+        'Wobbly Freeze Range Start': lambda start: str(start)
+    },
+    'wobbly_freeze_end': {
+        'Wobbly Freeze Range End': lambda end: str(end)
+    },
+    'wobbly_freeze_replacement': {
+        'Wobbly Freeze Range Replacement': lambda replacement: str(replacement)
+    },
+    'wobbly_ftf': {
+        'Wobbly Fix Telecined Fades': [
+            'Yes',
+            'No'
+        ]
+    },
+    'wobbly_fif': {
+        'Wobbly Fix Interlaced Fades': [
+            'Yes',
+            'No'
+        ]
+    },
+    'wobbly_orphan_deinterlace': {
+        'Wobbly Orphan Field Deinterlaced': lambda m: f"Yes ({m})" if not isinstance(m, int) or m >= 0 else "No"
+    }
+}
+
+# VMAF-related properties
+vmaf_props_lut: dict[str, dict[str, Callable[[Any], str]]] = {
+    'ciede2000': {
+        'CIEDE2000': lambda ciede2000: _handle_nan(ciede2000)
+    },
+    'float_ssim': {
+        'SSIM (Float)': lambda float_ssim: _handle_nan(float_ssim)
+    },
+    'float_ms_ssim': {
+        'MS-SSIM (Float)': lambda float_ms_ssim: _handle_nan(float_ms_ssim)
+    },
+    'psnr_y': {
+        'PSNR Luma (Y)': lambda psnr_y: _handle_nan(psnr_y)
+    },
+    'psnr_cb': {
+        'PSNR Chroma Blue (Cb)': lambda psnr_cb: _handle_nan(psnr_cb)
+    },
+    'psnr_cr': {
+        'PSNR Chroma Red (Cr)': lambda psnr_cr: _handle_nan(psnr_cr)
+    },
+    'psnr_hvs': {
+        'PSNR (HVS)': lambda psnr_hvs: _handle_nan(psnr_hvs)
+    },
+    'psnr_hvs_y': {
+        'PSNR (HVS) Luma (Y)': lambda psnr_hvs_y: _handle_nan(psnr_hvs_y)
+    },
+    'psnr_hvs_cb': {
+        'PSNR (HVS) Chroma Blue (Cb)': lambda psnr_hvs_cb: _handle_nan(psnr_hvs_cb)
+    },
+    'psnr_hvs_cr': {
+        'PSNR (HVS) Chroma Red (Cr)': lambda psnr_hvs_cr: _handle_nan(psnr_hvs_cr)
+    },
+}
+
+enum_props_lut = dict([
+    _create_enum_props_lut(enum, name)
+    for enum, name in list[tuple[type[PropEnum], str]]([
+        (FieldBased, 'Field Type'),
+        (Matrix, 'Matrix'),
+        (Transfer, 'Transfer'),
+        (Primaries, 'Primaries'),
+        (ChromaLocation, 'Chroma Location'),
+        (ColorRange, 'Color Range')
+    ])
+])
+
+# Combine all the LUTs
+frame_props_lut = dict[str, Any](
+    **basic_frame_props_lut,
+    **vssource_props_lut,
+    **vivtc_props_lut,
+    **tivtc_props_lut,
+    **dmetrics_props_lut,
+    **packet_size_props_lut,
+    **wobbly_props_lut,
+    **vmaf_props_lut,
+) | enum_props_lut

597b980d0104aa71542b406156260456f6d45746
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-12-06 08:01:41 +0100

    Bump minor version

--- vspreview/_metadata.py
+++ vspreview/_metadata.py
@@ -1,6 +1,6 @@
 """Previewer for VapourSynth scripts"""
 
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 
 __author_name__, __author_email__ = 'Endilll', ''
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'

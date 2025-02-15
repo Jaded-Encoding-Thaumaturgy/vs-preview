eecb61d78be584434bc359d3ece1f34347bd04b9
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2025-02-14 22:16:16 -0500

    Bump minor version

--- vspreview/_metadata.py
+++ vspreview/_metadata.py
@@ -1,6 +1,6 @@
 """Previewer for VapourSynth scripts"""
 
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 
 __author_name__, __author_email__ = 'Endilll', ''
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'

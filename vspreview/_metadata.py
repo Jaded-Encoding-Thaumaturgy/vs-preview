61544e15229d0673e89605dc054804ba1b11ed8e
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-07-21 19:39:14 +0000

    Bump version

--- vspreview/_metadata.py
+++ vspreview/_metadata.py
@@ -1,6 +1,6 @@
 """Previewer for VapourSynth scripts"""
 
-__version__ = '0.14.0'
+__version__ = '0.15.0'
 
 __author_name__, __author_email__ = 'Endilll', ''
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'

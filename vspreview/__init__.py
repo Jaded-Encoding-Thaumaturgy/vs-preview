03cd612aba3a3767631fac97c84b3b19abda9e87
Vardë <Ichunjo@users.noreply.github.com>
2025-11-22 01:15:41 +0100

    Use Git-derived version (#267)
    * update .gitignore
    
    * use Git-derived version
    
    * remove useless workflows file
    
    * set up dev environment

--- vspreview/__init__.py
+++ vspreview/__init__.py
@@ -9,3 +9,13 @@ from .api import is_preview
 
 if is_preview():
     from .init import main
+
+__version__: str
+__version_tuple__: tuple[int | str, ...]
+
+
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+    __version_tuple__ = (0, 0, 0, "+unknown")
